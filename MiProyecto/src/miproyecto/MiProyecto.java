77fc8d1bd9a2f885952d9bcc3e42b6a26d684ce4
Leo Hernandez <micorreo@gmail.com>
2024-06-21 11:56:21 -0400

    La hice de oro!

--- MiProyecto/src/miproyecto/MiProyecto.java
+++ MiProyecto/src/miproyecto/MiProyecto.java
@@ -16,6 +16,10 @@ public class MiProyecto {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Estaba facil");
+        System.out.println("La embarre");
+        System.out.println("La embarre");
+        System.out.println("La embarre");
+        System.out.println("La embarre");
     }
     
 }
